925bd805df038d684ae823f6dc5a663c70d085d7
abhimac9576 <abhimac9576@gmail.com>
2023-10-16 23:04:24 +0530

    Project Completed

---
+++ blogProject/src/components/index.js
@@ -0,0 +1,19 @@
+import Header from "./header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard"
+import Select from "./Select"
+import Login from "./Login"
+import SignUp from "./SignUp"
+import Protected from "./AuthLayout"
+import RTE from "./RTE";
+import PostForm from "./PostForm/PostForm"
+import AuthLayout from "./AuthLayout"
+
+
+
+export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,PostCard,Select,Login,SignUp,Protected,RTE, PostForm,AuthLayout} 
